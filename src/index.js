acabf29657b4bb48e437350bb298742039545d6f
Joshua Connor <jconno26@uic.edu>
2019-11-18 11:30:53 -0600

    map is running

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('HELLO!')
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiamNvbm5vcjEwMjciLCJhIjoiY2szNHA1aWNuMTd5OTNuczVsdW4wNmxhaCJ9.t8Y8CRJgqfo7AGBSwB9AOA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
